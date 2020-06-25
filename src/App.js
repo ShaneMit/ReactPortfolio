79ad474ed9c0d9eed3e2b5645a530956c32f5877
ShaneMit <shanemit@cox.net>
2020-06-24 18:09:11 -0700

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Switch, Link, Route, BrowserRouter as Router } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
-
+const App = () => {
+  return (
+    <Navbar/>
+  )
+}
 
 
 export default App;
