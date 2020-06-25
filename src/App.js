1424800911b9f51fa28e53ec2e3900badab37ea1
ShaneMit <shanemit@cox.net>
2020-06-24 18:32:03 -0700

    Created Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Switch, Link, Route, BrowserRouter as Router } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
+   <> 
     <Navbar/>
+    <Footer/>
+   </>
   )
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <nav className="navbar fixed-bottom navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">Thank you for visiting!</span>
+      </nav>
+    </>
+  )
+}
+
+export default Footer
