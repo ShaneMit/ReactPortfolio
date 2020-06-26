e4d858a19103b42a7bdfb78804f11085696557c3
ShaneMit <shanemit@cox.net>
2020-06-25 18:00:36 -0700

    added project component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Link, Route, BrowserRouter as Router } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Project from './components/Project'
 
 const App = () => {
   return (
    <> 
     <Navbar/>
+    <Project/>
     <Footer/>
    </>
   )
---
+++ src/components/Project/Project.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Project = () => {
+  return(
+    <>
+      <div className="card" style={{width: '18rem'}}>
+        <img src="..." className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h1>Project Title</h1>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <link rel="stylesheet" href=""/>
+            <link rel="stylesheet" href=""/>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default Project
