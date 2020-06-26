af331d1185a3deb9c296c42e5c41beb272794b64
ShaneMit <shanemit@cox.net>
2020-06-25 18:47:51 -0700

    Created paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Switch, Link, Route, BrowserRouter as Router } from 'react-router-dom'
+import AboutMe from './pages/About Me'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Project from './components/Project'
@@ -9,7 +12,17 @@ const App = () => {
     <Router>
       <div>
         <Navbar/>
+        
         <Switch>
+          <Route exact path="/">
+            <AboutMe/>
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio/>
+          </Route>
+          <Route path="/contact">
+            <Contact/>
+          </Route>
 
         </Switch>
         <Footer/>
