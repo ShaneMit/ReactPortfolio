00c7b1e87923ef412f73273ec1289e053c35cb32
ShaneMit <shanemit@cox.net>
2020-06-26 22:53:21 -0700

    Added Contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import ContactForm from '../../components/ContactForm'
 
 const Contact = () => {
   return (
-    <h1>Contact page</h1>
+    <>
+      <div className="container" style={{padding: '30px'}}>
+        <h1>Contact Me</h1>
+        <ContactForm/>
+      </div>
+    </>
   )
 }
 
