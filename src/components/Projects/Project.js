323e79ce143b23687811b7f1c3a8a034d355a745
ShaneMit <shanemit@cox.net>
2020-06-26 21:13:35 -0700

    added Photos and all project info

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Project = props => {
   return(
@@ -9,8 +8,9 @@ const Project = props => {
           <div className="card-body">
             <h1>{props.title}</h1>
             <p className="card-text">{props.description}</p>
-            <a href={props.deployedLink}>{props.title}</a>
-            <a href={props.githubLink}> GitHub</a>
+            <a href={props.deployedLink} target="_blank">{props.title}</a>
+            <br/>
+            <a href={props.githubLink} target="_blank"> GitHub Repo</a>
           </div>
       </div>
     </>
