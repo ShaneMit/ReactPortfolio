b27ffb73e43a44542a242680064776e7f81e801d
ShaneMit <shanemit@cox.net>
2020-06-29 23:56:59 -0700

    Deleted build folder

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron container" style={{padding: '10px'}}>
+      <h1 className="display-4">Shane Mitchell</h1>
+      <p className="lead">I am a web developer living in Orange County, California. I love film, photography, gaming, and
+          hiking. I graduated from California State University Fullerton where I double majored in Cinema and Television Arts and American Studies. I currently work in real estate on the admin side and am looking for a change of career. I enjoy creative pursuits and I am looking forward to the exciting challenge of a career  in web development.</p>
+
+      
+
+    </div>
+  )
+}
+
+export default Jumbotron
