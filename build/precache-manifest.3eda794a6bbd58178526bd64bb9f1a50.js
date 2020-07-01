ab34805fac5c4c3c5abf402d4595038c848e4386
ShaneMit <shanemit@cox.net>
2020-06-30 17:30:24 -0700

    Added github pages

---
+++ build/precache-manifest.3eda794a6bbd58178526bd64bb9f1a50.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c93455a43652bdc5993d362ef8cafa6",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "5b615b4ebe2f67e0f815",
+    "url": "/ReactPortfolio/static/js/2.66a33cbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.66a33cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51cc6be5ea26b4c6abe",
+    "url": "/ReactPortfolio/static/js/main.aab8178e.chunk.js"
+  },
+  {
+    "revision": "0114f5eceb8464d5d956",
+    "url": "/ReactPortfolio/static/js/runtime-main.5b3ce1ae.js"
+  },
+  {
+    "revision": "e76be3a073576b3e267eaa61da542917",
+    "url": "/ReactPortfolio/static/media/Arch-Rock.e76be3a0.jpg"
+  },
+  {
+    "revision": "f3dbf93f4b6a6bf1181e33fd827f9483",
+    "url": "/ReactPortfolio/static/media/BudgetTracker.f3dbf93f.png"
+  },
+  {
+    "revision": "337e0b201eb0fd3bc5e7b4fdf5d7bd33",
+    "url": "/ReactPortfolio/static/media/CoZone.337e0b20.jpg"
+  },
+  {
+    "revision": "0bc752cc3423e252b0d0e7e51db9000b",
+    "url": "/ReactPortfolio/static/media/ComingSoon.0bc752cc.png"
+  },
+  {
+    "revision": "313abe21e0147aa4bb7320bb0712d4df",
+    "url": "/ReactPortfolio/static/media/FitnessTracker.313abe21.png"
+  },
+  {
+    "revision": "1f6192a390320e7299a64c0571b0ddee",
+    "url": "/ReactPortfolio/static/media/Joshua-Tree.1f6192a3.jpg"
+  },
+  {
+    "revision": "5797f98f8f6efa4003d9d546b805d491",
+    "url": "/ReactPortfolio/static/media/Listivity.5797f98f.png"
+  },
+  {
+    "revision": "9348063988dd1130698ce8a5d7551fb0",
+    "url": "/ReactPortfolio/static/media/Monterey.93480639.jpg"
+  },
+  {
+    "revision": "73097e050b8183df45ad7215e550eb2a",
+    "url": "/ReactPortfolio/static/media/NoteTaker.73097e05.png"
+  }
+]);
